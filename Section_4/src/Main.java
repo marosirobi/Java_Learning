3f95e296139b0655b740e34f6b724fa1f49fdfa2
marosirobi <marosirobi666@gmail.com>
2025-05-29 15:57:25 +0200

    section_4

--- Section_4/src/Main.java
+++ Section_4/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //1.
+        //ternary operators
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? true : false;
+        String s = isDomestic ? "This car is domestic" : "This car is not domestic";
+        if(isDomestic)
+           System.out.println(s);
+
+        //2.
+        double a = 20.0d;
+        double b = 80.0d;
+        double sum = (a + b) * 100.0d;
+        System.out.println(sum);
+        double remain = sum % 40.0d;
+        System.out.println(remain);
+        boolean isNoRemain = (remain == 0) ? true : false;
+        System.out.println(isNoRemain);
+        if(!isNoRemain)
+            System.out.println("got some remainder");
     }
 }
