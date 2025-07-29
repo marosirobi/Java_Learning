7eb03c9a5e3965842b4a5602bd70328a0444dc7b
marosirobi <marosirobi666@gmail.com>
2025-07-29 19:57:06 +0200

    Section_13

--- Section_13/src/model/nesting/Main.java
+++ Section_13/src/model/nesting/Main.java
@@ -1,6 +1,7 @@
 package model.nesting;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -30,11 +31,51 @@ public class Main {
         var comp = new StoreEmployee().new StoreComparator<>();
         storeEmployees.sort(comp);
         printList(storeEmployees);
+
+        System.out.println("With Pig Latin Names");
+        addPigLatinName(storeEmployees);
     }
 
     public static void printList(List<? extends Employee> list){
         for(var item : list){
             System.out.println(item);
+        }
+    }
+
+    public static void addPigLatinName(List<? extends StoreEmployee> list){
+
+        final String lastName = "Piggy";
+
+        class DecoratedEmployee extends StoreEmployee implements Comparable<DecoratedEmployee>{
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName + " " + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + " " + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size());
+
+        for(var employee : list){
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin,employee));
+        }
+        newList.sort(null);
+        for(var dEmployee : newList){
+            System.out.println("%-10s".formatted(dEmployee.originalInstance.getName())+ " " + dEmployee.pigLatinName);
         }
     }
 }
