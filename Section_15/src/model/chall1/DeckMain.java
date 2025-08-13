9c8412282833ce16465bc4f61e2c194e35016249
marosirobi <marosirobi666@gmail.com>
2025-08-13 18:26:13 +0200

    Section_15

---
+++ Section_15/src/model/chall1/DeckMain.java
@@ -0,0 +1,41 @@
+package model.chall1;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class DeckMain {
+
+    public static void main(String[] args) {
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Suit.HEART,'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray),"Aces of Hearts",1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards,aceOfHearts);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts,"Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs,"Aces of Hearts", 1);
+
+        Collections.addAll(cards,cardArray);
+        Card.printDeck(cards,"Card Collection with Aces added", 1);
+        Collections.addAll(cards,cardArray);
+        Card.printDeck(cards,"Card Collection with Aces added", 2);
+
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards,"Card Collection with Kings copied", 2);
+
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings",1);
+    }
+}
---
+++ Section_15/src/model/chall1/Suit.java
@@ -0,0 +1,14 @@
+package model.chall1;
+
+public enum Suit {
+    CLUB,
+    DIAMOND,
+    HEART,
+    SPADE;
+
+    public char getImage(){
+        return (new char[]{9827,9830,9829,9824})[this.ordinal()];
+    }
+
+
+}
