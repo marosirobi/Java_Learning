b4047f21b7821aa3e1b2d403311f998da36af19a
marosirobi <marosirobi666@gmail.com>
2025-08-22 21:06:50 +0200

    Section_15

--- Section_15/src/model/chall2/BlackJack.java
+++ Section_15/src/model/chall2/BlackJack.java
@@ -1,77 +1,108 @@
 package model.chall2;
 
 import model.chall1.Card;
+import model.chall1.Suit;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 
 public class BlackJack {
 
-    private static int yourPoints = 0;
-    private static int dealerPoints = 0;
+    private int yourPoints;
+    private int dealerPoints;
     private static int bound = 51;
-    private static final int rand = new Random().nextInt(bound);
+    private static int rand = new Random().nextInt(bound);
 
-    public static List<Card> createDeck(){
-        return Card.getStandardDeck();
+    private List<Card> yourHand,dealerHand;
+    private List<Card> remainingCards;
+    private final List<Card> deck = Card.getStandardDeck();
+
+    public BlackJack() {
+        yourPoints = 0;
+        dealerPoints = 0;
+        yourHand = new ArrayList<>();
+        dealerHand = new ArrayList<>();
+        remainingCards = deck.subList(0,deck.size()-1);
     }
 
-    public static void shuffleDeck(List<Card> list){
-        Collections.shuffle(list);
+    public void startGame(){
+        Collections.shuffle(deck);
+        dealYourHand();
+        dealYourHand();
+        dealDealerHand();
+        printHands();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Do you wanna deal?");
+        if(sc.next().equals("Y")){
+            dealYourHand();
+        }else{
+            System.out.println("who won?");
+            if(sc.next().equals("Y")){
+                whoWon();
+            }
+        }
+
+
     }
 
-    public static void dealYourHand(List<Card> list,List<Card> yourHand){
 
-        if(Collections.disjoint(list,yourHand)) {
-            Card card = list.get(rand);
+    public void dealYourHand(){
+
+        if(Collections.disjoint(remainingCards,yourHand)) {
+            Card card = remainingCards.get(rand);
 
             yourHand.add(card);
             yourPoints += card.rank()+2;
-            list.remove(card);
+            remainingCards.remove(card);
             bound--;
         }
         else{
-            dealYourHand(list,yourHand);
+            dealYourHand();
+        }
+
+        if(yourPoints > 21){
+            whoWon();
         }
     }
 
-    public static void dealDealerHand(List<Card> list,List<Card> dealerHand){
-        if(Collections.disjoint(list,dealerHand)) {
-            Card card = list.get(rand);
+    public void dealDealerHand(){
+        if(dealerHand.isEmpty())
+            dealerHand.add(new Card(Suit.DIAMOND,"0",0));
+        if(Collections.disjoint(remainingCards,dealerHand)) {
+            Card card = remainingCards.get(rand);
             dealerHand.add(card);
             dealerPoints += card.rank()+2;
-            list.remove(card);
+            remainingCards.remove(card);
             bound--;
         }
         else{
-            dealYourHand(list,dealerHand);
+            dealYourHand();
         }
     }
 
 
 
-    public static void printHands(List<Card> yourHand,List<Card> dealerHand){
+    public void printHands(){
         System.out.println("-".repeat(30));
         System.out.println(yourHand +": " + yourPoints);
         System.out.println(dealerHand +": " + dealerPoints);
-        whoWon();
 
     }
 
-    public static void whoWon(){
-        if(yourPoints > dealerPoints){
+    public void whoWon(){
+        dealDealerHand();
+        dealerHand.removeFirst();
+        Collections.rotate(dealerHand,1);
+        printHands();
+        if((yourPoints > dealerPoints && yourPoints <= 21) || dealerPoints > 21 ){
             System.out.println("you won");
             return;
         }else if(yourPoints == dealerPoints){
             System.out.println("equal");
             return;
-        }
-        System.out.println("dealer won");
+        }else
+            System.out.println("dealer won");
     }
-
-
-
 
 }
