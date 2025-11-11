f5311f4073ea1ddd1f0ae8c0dc37450d090b3bee
marosirobi <marosirobi666@gmail.com>
2025-11-11 18:08:22 +0100

    Section_20

---
+++ Section_20/src/Chall2/Chall2.java
@@ -0,0 +1,70 @@
+package Chall2;
+
+import fileListing.FileListingMain;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class Chall2 {
+
+    public static void main(String[] args) {
+        try {
+            generateFile(Path.of("index.txt"), Path.of("public"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void generateFile(Path filename, Path path) throws IOException {
+
+        Path target = Path.of(path+"/"+filename);
+
+        if(Files.isDirectory(path)){
+
+            try(BufferedWriter writer = Files.newBufferedWriter(target)){
+                try(var children = Files.list(path)){
+                    children.toList().forEach(
+                            p ->{
+                                try {
+                                    writer.write(listDir(p));
+                                    if(!Files.exists(Path.of(p+"/"+filename)) ){
+                                        Chall2.generateFile(filename, p);
+                                    }
+
+
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                    );
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+
+    public static String listDir(Path path){
+        StringBuilder list = new StringBuilder();
+        try (Stream<Path> paths = Files.find(path, Integer.MAX_VALUE,
+                (p,attr) -> attr.isRegularFile() || attr.isDirectory()
+        )) {
+            paths
+                    .forEach(s -> {
+                        try {
+                            list.append(s).append(" --> ").append(Files.getAttribute(s,"lastModifiedTime")).append("\n");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return list.toString();
+    }
+}
--- Section_20/src/managingFiles/ManagingFiles.java
+++ Section_20/src/managingFiles/ManagingFiles.java
@@ -1,9 +1,11 @@
 package managingFiles;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class ManagingFiles {
 
@@ -32,13 +34,105 @@ public class ManagingFiles {
         Path fileDir = Path.of("files");
         Path resourceDir = Path.of("resources");
         try{
-            Files.copy(fileDir, resourceDir);
+            recurseDelete(resourceDir);
+            recurseCopy(fileDir, resourceDir);
             System.out.println("Directory copied to " + resourceDir);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+//        try(BufferedReader reader = new BufferedReader(
+//                new FileReader("files//students.json"));
+//        PrintWriter writer = new PrintWriter("students-backup.json")){
+//
+//            reader.transferTo(writer);
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
+        String urlString = "https://api.census.gov/data/2019/pep/charagegroups?get=NAME,POP&for=state:*";
+        URI uri = URI.create(urlString);
+        try(var urlInputStream = uri.toURL().openStream()){
 
+            urlInputStream.transferTo(System.out);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Path jsonPath = Path.of("USPopulationByState.txt");
+        try(var reader = new InputStreamReader(uri.toURL().openStream());
+            var writer = Files.newBufferedWriter(jsonPath)){
+
+            reader.transferTo(writer);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(var reader = new InputStreamReader(uri.toURL().openStream());
+            PrintWriter writer = new PrintWriter("USPopulationByState.csv")){
+
+            reader.transferTo(new Writer(){
+                @Override
+                public void write(char[] cbuf, int off, int len) throws IOException {
+                    String jsonString = new String(cbuf,off,len).trim();
+                    jsonString = jsonString.replace("[", " ").trim();
+                    jsonString = jsonString.replaceAll("\\]", "");
+
+                    writer.write(jsonString);
+                }
+
+                @Override
+                public void flush() throws IOException {
+                    writer.flush();
+                }
+
+                @Override
+                public void close() throws IOException {
+                    writer.close();
+                }
+            });
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void recurseCopy(Path source, Path target) throws IOException {
+        Files.copy(source,target, StandardCopyOption.REPLACE_EXISTING);
+        if(Files.isDirectory(source)){
+            try(var children = Files.list(source)){
+                children.toList().forEach(
+                        p-> {
+                            try {
+                                ManagingFiles.recurseCopy(p, target.resolve(p.getFileName()));
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+            }
+        }
+
+    }
+
+    private static void recurseDelete(Path target) throws IOException {
+
+        if(!Files.exists(target)) return;
+
+        if(Files.isDirectory(target)){
+            try(var children = Files.list(target)){
+                children.toList().forEach(
+                        p-> {
+                            try {
+                                ManagingFiles.recurseDelete(p);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });}
+        }
+        Files.delete(target);
     }
 }
